63ff1ec36a2116b3d33798c313b9f8744e18a3c4
Joe Roe <jroeuk@gmail.com>
2018-09-26 10:37:55 +0200

    added linked to github

--- main.inc.php
+++ main.inc.php
@@ -4,6 +4,7 @@ Version: 0.1
 Plugin Name: 3dhop
 Plugin URI: // link to the Piwigo extension gallery
 Author: joeroe
+Author URI: https://github.com/joeroe
 Description: Plugin for viewing 3D models (.obj) using 3DHOP <http://www.3dhop.net>
 */
 
